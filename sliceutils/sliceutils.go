9d5f7238f3ff9aad3961df6b6576f2dcd255cc32
Jian Yuan Lee <jianyuan@gmail.com>
2024-12-09 00:03:35 +0000

    feat: sliceutils.Map

---
+++ sliceutils/sliceutils.go
@@ -0,0 +1,10 @@
+package sliceutils
+
+// Map applies the function f to each element in the slice s and returns a new slice containing the results.
+func Map[T, R any](f func(T) R, s []T) []R {
+	result := make([]R, len(s))
+	for i, v := range s {
+		result[i] = f(v)
+	}
+	return result
+}
